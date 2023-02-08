49ad3d1269fda7a28c4fff47298e6cc6a541fb99
jonathan kalonga <jokalmwamba@gmail.com>
2023-02-08 22:00:52 +0200

    add attribute to amphibian model

--- app/src/main/java/com/example/animalsamphibians/model/AmphibianModel.kt
+++ app/src/main/java/com/example/animalsamphibians/model/AmphibianModel.kt
@@ -1,4 +1,16 @@
 package com.example.animalsamphibians.model
 
-class AmphibianModel {
-}
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AmphibianModel (
+    val name: String,
+
+    val type: String,
+
+    val description: String,
+
+    @SerialName(value = "img_src")
+    val imgSrc: String
+    )
