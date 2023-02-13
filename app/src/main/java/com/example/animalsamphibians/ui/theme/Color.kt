72fe21d4340cc165a0f17f3a65d770e93455c8f7
jonathan kalonga <jokalmwamba@gmail.com>
2023-02-13 16:41:02 +0200

    add color of our app theme

--- app/src/main/java/com/example/animalsamphibians/ui/theme/Color.kt
+++ app/src/main/java/com/example/animalsamphibians/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.example.animalsamphibians.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+//val Purple200 = Color(0xFFBB86FC)
+//val Purple500 = Color(0xFF6200EE)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
+val Grey = Color(0xFFD1D1D1)
+val Green = Color(0xFF50AE55)
+val White = Color(0XFFFFFFFF)
+val Black = Color(0XFF000000)
+val Mve = Color(0xFF5158AD)
