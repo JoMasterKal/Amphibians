561146d55a654211d1972fa1c534cd87f998aa51
jonathan kalonga <jokalmwamba@gmail.com>
2023-03-18 16:19:59 +0200

    create ApiServices

--- app/src/main/java/com/example/animalsamphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/animalsamphibians/network/AmphibianApiService.kt
@@ -1,2 +1,29 @@
 package com.example.animalsamphibians.network
 
+import com.example.animalsamphibians.model.AmphibianModel
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import kotlinx.serialization.json.Json
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface AmphibianApiService {
+    @GET("amphibians")
+    suspend fun getAmphibians(): List<AmphibianModel>
+
+}
+
+object AmphibianApi {
+    val retrofitService : AmphibianApiService by lazy {
+        retrofit.create(AmphibianApiService::class.java)
+    }
+}
